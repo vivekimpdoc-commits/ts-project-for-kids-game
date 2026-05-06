bcbbb2819b5e61148424caf656ba75104211ddd9
vivek kumar verma <vivek.impdoc@gmail.com>
2026-05-06 15:49:00 +0530

    yes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173,
+    host: '0.0.0.0',
+  },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+  }
 })
